083435717386ad40b73d0002a74de3c401c4bfc0
Phoreal71 <42096133+Phoreal71@users.noreply.github.com>
2019-02-06 15:47:00 -0500

    Delete HourlyS.java

--- Project 3/HourlyS.java
+++
@@ -1,9 +0,0 @@
-public interface HourlyS extends Supervisor{
-  double getHourlyRate();
-  
-  void setHourlyRate(double rate);
-  
-  double getHoursWorked();
-  
-  void setHoursWorked(double hoursWorked);
-}
